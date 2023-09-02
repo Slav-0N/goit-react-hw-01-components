22601181d91046598ecf47c7b3507caa2183687a
Slav-0N <SOVILGO@gmail.com>
2023-09-02 16:45:45 +0300

    untracked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from "./Profile/Profile";
+import user from "../"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile  username="user.username" />
     </div>
   );
 };
