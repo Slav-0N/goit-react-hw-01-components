3ad806db5228b1add0118493181f5daab8fc95dc
Slav-0N <SOVILGO@gmail.com>
2023-09-02 22:17:48 +0300

    френдліст готовий

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+export const FriendList = ({ friends }) => {
+  console.log(friends)
+  return (
+    <ul className="friend-list">
+      <FriendListItem frends={friends} />
+    </ul>
+  )
+}
+
+const FriendListItem = ({ frends }) => {
+  return frends.map(friend => {
+    return (
+      <li className="item" key={friend.id}>
+        <span className="status">{ friend.isOnline }</span>
+        <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+        <p className="name">{ friend.name }</p>
+      </li>
+    )
+  })
+}
