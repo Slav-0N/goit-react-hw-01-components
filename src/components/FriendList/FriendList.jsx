1de21fadedd216bff0b4c827dc57c606de74bc2e
Slav-0N <SOVILGO@gmail.com>
2023-09-04 12:30:13 +0300

    мапнув друзів всередині компонента FrienfList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,21 +5,21 @@ import { ListWithFriends, Markered } from './FriendList.styled'
 export const FriendList = ({ friends }) => {
   return (
     <ListWithFriends className="friend-list">
-      <FriendListItem frends={friends} />
+      {friends.map(friend => {
+        return (
+          <li className="item" key={friend.id}>
+            <Markered markering={friend.isOnline.toString()}>{friend.isOnline}</Markered>
+
+            <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+            <p className="name">{friend.name}</p>
+          </li>
+          )
+        })
+      }
+
+
     </ListWithFriends>
   )
-};
-
-const FriendListItem = ({ frends }) => {
-  return frends.map(friend => {
-    return (
-      <li className="item" key={friend.id}>
-        <Markered markering={friend.isOnline.toString()}>{friend.isOnline}</Markered>
-        <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-        <p className="name">{friend.name}</p>
-      </li>
-    )
-  })
 };
 
 FriendList.propTypes = {
